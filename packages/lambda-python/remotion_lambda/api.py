48eb495156ce46a202ec6049a824bf31c8b253a5
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2024-05-28 19:14:49 +1000

    initial work

---
+++ packages/lambda-python/remotion_lambda/api.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+class S3Utils:
+
+     # pylint: disable=too-many-arguments
+    def __init__(self, region, serve_url, function_name, access_key=None, secret_key=None):
+        """
+        Initialize the RemotionClient.
+
+        Args:
+            region (str): AWS region.
+            serve_url (str): URL for the Remotion service.
+            function_name (str): Name of the AWS Lambda function.
+            access_key (str): AWS access key (optional).
+            secret_key (str): AWS secret key (optional).
+        """
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.region = region
+        self.serve_url = serve_url
+        self.function_name = function_name
+        self.s3 = boto3.client("s3")
+        
+
+
+    def __init__(self, bucket_name):
+        self.bucket = boto3.resource("s3").Bucket(bucket_name)
+
+    def load(self, key):
+        return json.load(self.bucket.Object(key=key).get()["Body"])
+
+    def dump(self, key, obj):
+        return self.bucket.Object(key=key).put(Body=json.dumps(obj))
